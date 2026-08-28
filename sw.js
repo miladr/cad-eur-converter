8469b74919d27edd7f1cb033c486a4cbc3d5b0d3
Milad Reyhani <miladr@gmail.com>
2026-08-28 16:51:41 -0400

    Refresh cache for version 1.1.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'toronto-tools-v1.1.0';
+const CACHE_NAME = 'toronto-tools-v1.1.1';
 const ASSETS = ['./', './index.html', './pitcher.html', './manifest.json'];
 
 self.addEventListener('install', event => {
